94abe972a79918bd124a33c1a2ec3076eaf246f5
VarunJindal283 <102215376+VarunJindal283@users.noreply.github.com>
2023-12-05 00:35:12 +0530

    Time: 3 ms (62.87%), Space: 42.1 MB (16.17%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
@@ -3,13 +3,18 @@ class Solution {
         int p1=0;
         int p3=2;
         String max="";
-        for(int i=1;i<num.length()-1;i++,p1++,p3++){
+        for(int i=1;i<num.length()-1;i++){
             if(num.charAt(p1)==num.charAt(i) && num.charAt(i)==num.charAt(p3)){
                 String ans=num.substring(p1,p3+1);
-                if(max.equals("") || Integer.valueOf(ans)>Integer.valueOf(max)){
+                if(max.equals("")){
+                    max=ans;
+                }
+                else if(Integer.valueOf(ans)>Integer.valueOf(max)){
                     max=ans;
                 }
             }
+            p1++;
+            p3++;
         }
         return max;
     }
