688474fe27aeaaa0bfa9d8bcb3979a15b7fa3f9f
VarunJindal283 <102215376+VarunJindal283@users.noreply.github.com>
2023-12-08 13:04:47 +0530

    Time: 1178 ms (12.72%), Space: 52.3 MB (66.67%) - LeetHub

---
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> findPrimePairs(int n) {
+        List<List<Integer>> ans=new ArrayList<>();
+        if(n%2!=0){
+            List<Integer> list=new ArrayList<>();
+            if(n-2>=2 && isPrime(n-2)){
+                list.add(2);
+                list.add(n-2);
+                ans.add(list);
+            }
+            return ans;
+        }
+        else{
+            for(int i=1;i<=n/2;i++){
+                if(isPrime(i) && isPrime(n-i)){
+                    List<Integer> list=new ArrayList<>();
+                    list.add(i);
+                    list.add(n-i);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
+    }
+    public boolean isPrime(int num){
+        if(num<=1)
+        {
+            return false;
+        }
+       for(int i=2;i<=Math.sqrt(num);i++)
+       {
+           if((num%i)==0)
+               return  false;
+       }
+       return true;
+    }
+}
